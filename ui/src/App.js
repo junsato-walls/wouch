c77fda6d74881b1f3c514605e6f8505641126644
kazumakato <k.kato@walls-inc.com>
2022-08-27 14:11:38 +0900

    first commit

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route  } from 'react-router-dom';
 import TimeCard from './view/timecard/timecard';
+import Input from './view/timecard/tc001_input/input';
 import Admin from './view/admin/admin';
 import NoMatch from './view/nomatch';
 
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
       <Routes>
           <Route path="/" element={<TimeCard />} />
+          <Route path="/input" element={<Input />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="*" element={<NoMatch />} />
       </Routes>
