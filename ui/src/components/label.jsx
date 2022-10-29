9a98c0573233b98d3639bdd0f74e174cded68819
kazumakato <k.kato@walls-inc.com>
2022-10-29 14:37:33 +0900

    画面編集

---
+++ ui/src/components/label.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import styled from 'styled-components'
+
+const Label = styled.div`
+    width: 100%;
+    font-weight: bold;
+    font-size:2.5em
+`;
+
+export default Label
