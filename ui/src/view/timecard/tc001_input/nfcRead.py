fccc78bd029ea03a5e3faf0c8021692ed1c50e33
kazumakato <k.kato@walls-inc.com>
2022-12-10 13:10:53 +0900

    nfc読み取り.py

--- ui/src/view/timecard/tc001_input/nfcRead.py
+++ ui/src/view/timecard/tc001_input/nfcRead.py
@@ -3,6 +3,20 @@ import nfc
 import binascii
 import time
 from threading import Thread, Timer
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import signal
+import time
+
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+edgedriver = webdriver.Edge(executable_path="./msedgedriver.exe")
+baseURL = "http://localhost:3000"
+edgedriver.get(baseURL)
+time.sleep(3)
+
+# NFCIDMを入力するID名を取得
+elementText = edgedriver.find_element_by_id("nfc_input")
+# elementButton = edgedriver.find_element_by_id("SendID")
 
 # 待ち受けの1サイクル秒
 TIME_cycle = 10.0
@@ -24,22 +38,27 @@ def check_NFC():
     # USBに接続されたNFCリーダに接続してインスタンス化
     clf = nfc.ContactlessFrontend('usb')
 
-    mydict = {}
     while True:
         target_res = clf.sense(target_req_nfc, target_req_felica, iterations=int(
             TIME_cycle//TIME_interval)+1, interval=TIME_interval)
         if not target_res is None:
             tag = nfc.tag.activate(clf, target_res)
-            print('TAG type: ' + tag.type)
             # NFCタグに埋めたtextを読む
             records = tag.ndef.records
+            elementText.clear()
+
             for record in records:
                 print('NFC detected. record.text = ' + record.text)
                 # str()で変換するとユニコードオブジェクトにならない
-                key = str(record.text)
-                mydict[key] = key
+                # key = str(record.text)
+                # mydict[key] = key
+                num = record.text
+                elementText.send_keys(num)
+                elementText.clear()
 
-            print('sleep ' + str(TIME_wait) + ' seconds')
-            time.sleep(TIME_wait)
+                time.sleep(2)
+# print('sleep ' + str(TIME_wait) + ' seconds')
+# time.sleep(TIME_wait)
+
 
 check_NFC()
